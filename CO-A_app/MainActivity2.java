94870076fc2cc131ba5b70426220848719411b53
You Jung Hawn <u_egg@naver.com>
2021-05-17 23:27:35 +0900

    Create MainActivity2.java

---
+++ CO-A_app/MainActivity2.java
@@ -0,0 +1,27 @@
+package com.cookandroid.project6_1;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.os.Handler; // 자동으로 넘어가게 하기 위한 함수
+
+public class MainActivity2 extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main2);
+
+        Handler hand = new Handler();
+
+        hand.postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                Intent i = new Intent(MainActivity2.this, MainActivity.class); // 액티비티 지정
+                startActivity(i);
+                finish();
+            }
+        },2000); // 2초뒤에 자동으로 다음 액티비티로 넘어가게 설정
+    }
+}
